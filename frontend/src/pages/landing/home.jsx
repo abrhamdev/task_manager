96ea35395f34364748ec43ee22f949c8b1606c3f
Abrham A. <abrhamabebe564@gmail.com>
2025-08-22 18:08:55 +0300

    additional customization in landing page

--- frontend/src/pages/landing/home.jsx
+++ frontend/src/pages/landing/home.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import Footer from "../../components/landingpage/footer";
 import AboutUs from "../../components/landingpage/about";
+import Features2 from "../../components/landingpage/features2";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -16,14 +17,21 @@ const Home = () => {
       navigate("/dashboard/home");
     }
   });
+
   return (
     <>
       <Navbar />
-      <HeroSection />
-      <DescriptionSection />
-      <Features />
-      <AboutUs />
-      <Carousel />
+      <div className="relative overflow-hidden">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-blue-900 to-black opacity-95 z-0"></div>
+        <div className="relative z-10">
+          <HeroSection />
+          <DescriptionSection />
+          <Features2/>
+          <Features />
+          <AboutUs />
+          <Carousel />
+        </div>
+      </div>
       <Footer />
     </>
   );
